3d2d0cc99f5ec08769dc4cf09b17813c73cb0a27
CONGTRANV <56678165+congtranv@users.noreply.github.com>
2023-07-26 17:59:48 +0700

    update ivsr_log.h

--- include/offboard/ivsr_log.h
+++ include/offboard/ivsr_log.h
@@ -1,14 +1,18 @@
 #ifndef IVSR_LOG_H_
 #define IVSR_LOG_H_
+#include <iostream>
+#include <unistd.h>
 
 void ivsrLogInfo(const char* format, ...);
 void ivsrLogWarn(const char* format, ...);
 void ivsrLogDebug(const char* format, ...);
 void ivsrLogError(const char* format, ...);
+void ivsrLogProgress(const char* format, ...);
 
 #define IVSR_LOGI(...) ivsrLogInfo(__VA_ARGS__)
 #define IVSR_LOGW(...) ivsrLogWarn(__VA_ARGS__)
 #define IVSR_LOGD(...) ivsrLogDebug(__VA_ARGS__)
 #define IVSR_LOGE(...) ivsrLogError(__VA_ARGS__)
+#define IVSR_LOGP(...) ivsrLogProgress (__VA_ARGS__)
 
 #endif  // IVSR_LOG_H_
